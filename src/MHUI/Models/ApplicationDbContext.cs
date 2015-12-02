2f05798fbd090c7713f3e12ae82f3f9abe04ce3b
Phillip Hutchings <sitharus@sitharus.com>
2015-12-02 13:46:30 +1300

    Initial commit

---
+++ src/MHUI/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace MHUI.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var appEnv = (IApplicationEnvironment)CallContextServiceLocator.Locator.ServiceProvider.GetService(typeof(IApplicationEnvironment));
+            optionsBuilder.UseSqlite($"Data Source={appEnv.ApplicationBasePath}/mh.sqlite");
+        }
+    }
+}
---
+++ src/MHUI/Services/MessageServices.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace MHUI.Services
+{
+    // This class is used by the application to send Email and SMS
+    // when you turn on two-factor authentication in ASP.NET Identity.
+    // For more details see this link http://go.microsoft.com/fwlink/?LinkID=532713
+    public class AuthMessageSender : IEmailSender, ISmsSender
+    {
+        // Plug in your email service here to send an email.
+        public Task SendEmailAsync(string email, string subject, string message) => Task.FromResult(0);
+
+        // Plug in your SMS service here to send a text message.
+        public Task SendSmsAsync(string number, string message) => Task.FromResult(0);
+    }
+}
